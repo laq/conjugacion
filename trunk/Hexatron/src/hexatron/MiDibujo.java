2cb1535d38995b31045cd28f65e4ee80fc5e6ce2
laqp14@gmail.com <laqp14@gmail.com@23d8c49f-e412-a02e-2c2b-0a2be4b60954>
2011-02-20 02:06:13 +0000

    variables de ancho y alto

--- trunk/Hexatron/src/hexatron/MiDibujo.java
+++ trunk/Hexatron/src/hexatron/MiDibujo.java
@@ -9,7 +9,12 @@ import Logica_Hexatron.Hexatron;
 import java.awt.Component;
 import java.awt.Menu;
 import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.MenuShortcut;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.geom.Line2D;
 import javax.swing.JFrame;
 
@@ -18,16 +23,16 @@ import javax.swing.JFrame;
  * @author Ztiphen
  */
 public class MiDibujo extends JFrame{
-    
+
     Hexatron hexatron = new Hexatron();
     Lienzo lienzo = new Lienzo(hexatron.getMatriz());
-    
+
     public void dibujar()
     {
     this.add(lienzo);
     }
 
-  
+
     public static void main(String args[]) {
     int screenx =  Toolkit.getDefaultToolkit().getScreenSize().width;
     int screeny = Toolkit.getDefaultToolkit().getScreenSize().height;
@@ -37,8 +42,18 @@ public class MiDibujo extends JFrame{
     midibujo.setVisible(true);
     midibujo.setDefaultCloseOperation(EXIT_ON_CLOSE);
     MenuBar menubar= new MenuBar();
-    menubar.add(new Menu(""));
-    
+    Menu menu=new Menu("automata");
+    MenuItem menuItem=new MenuItem("Ancho", new MenuShortcut(KeyEvent.VK_A));
+    menuItem.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    menu.add(menuItem);
+    menubar.add(menu);
+
+
     midibujo.setMenuBar(menubar);
     }
 
