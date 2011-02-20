b1bbde5939b40fdc44888384fd2f1866fd1bb091
laqp14@gmail.com <laqp14@gmail.com@23d8c49f-e412-a02e-2c2b-0a2be4b60954>
2011-02-20 02:06:13 +0000

    variables de ancho y alto

--- Hexatron/src/Logica_Hexatron/Hexatron.java
+++ Hexatron/src/Logica_Hexatron/Hexatron.java
@@ -10,22 +10,59 @@ package Logica_Hexatron;
  * @author Ztiphen
  */
 public class Hexatron {
-    
-  private  int matriz[][] = new int[120][120];
+
+  private  int matriz[][];
+  private int ancho;
+  private int alto;
 
     public int[][] getMatriz() {
         return matriz;
     }
-    
+
     public Hexatron()
     {
-        for(int i =0; i<matriz.length; i++)
-            for(int j =0; j<matriz.length; j++)
-                matriz[i][j]=(int)(Math.random()*4)+1;
-        
+        matriz=new int[ancho][alto];
+        poblar();
 
     }
-    
-    
+
+
+
+    /**
+     * @return the ancho
+     */
+    public int getAncho() {
+        return ancho;
+    }
+
+    /**
+     * @param ancho the ancho to set
+     */
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    /**
+     * @return the alto
+     */
+    public int getAlto() {
+        return alto;
+    }
+
+    /**
+     * @param alto the alto to set
+     */
+    public void setAlto(int alto) {
+        this.alto = alto;
+    }
+
+    private void poblar() {
+          for(int i =0; i<matriz.length; i++)
+            for(int j =0; j<matriz.length; j++)
+                matriz[i][j]=(int)(Math.random()*4)+1;
+
+    }
+
+
 
 }
