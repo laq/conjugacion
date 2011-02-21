b1af06fcd548f64fcd57c9a6365c7f9e983edf15
laqp14@gmail.com <laqp14@gmail.com@23d8c49f-e412-a02e-2c2b-0a2be4b60954>
2011-02-21 04:20:02 +0000

    Animacion completa, y entradas para la misma

--- Hexatron/src/hexatron/Lienzo.java
+++ Hexatron/src/hexatron/Lienzo.java
@@ -11,54 +11,44 @@ import Logica_Hexatron.Vacio;
 import java.awt.BasicStroke;
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Shape;
 import java.awt.Stroke;
 import java.awt.geom.Line2D;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JPanel;
 
 /**
  * Clase que hereda de Canvas y sirve para dibujar una linea.
  */
-public class Lienzo extends Canvas {
+public class Lienzo extends JPanel implements Runnable {
 
     int xPoints[] = {5, 10, 10, 5, 0, 0};
     int yPoints[] = {0, 5, 10, 15, 10, 5};
     private Hexatron hexatron;
     int cont = 0;
+    Image offscreen;
+    Graphics gBuff;
+    private int generations=5;
 
     /**
      * Constructor. Hace que el tamaño del canvas sea 100x100 pixels.
      */
     public Lienzo(Hexatron hexatron) {
-
         this.hexatron = hexatron;
+    }  
 
-
-    }
-
-    /**
-     * Dibuja la última línea que se le haya pasado.
-     */
-    public void paint(Graphics g) {
-
-
-        g.setColor(Color.green);
-        update(g);
-
-
-
-        //g.drawLine(23, 3, 56, 5);
-    }
-
-    public void update(Graphics g) {
-
+    
+    @Override
+    public void paint(Graphics g) {        
         int pxwidth = this.getWidth();
         int pxheight = this.getHeight();
-        float width = pxwidth / hexatron.getAncho();
-        float height = pxheight / hexatron.getAlto();
+        float width = pxwidth / (hexatron.getAncho()+2);
+        float height = pxheight / (hexatron.getAlto()+2);
         float val = width > height ? height : width;
         int valHex = (int) (val / 2 - (val / 10));
         xPoints[0] = valHex;
@@ -102,13 +92,8 @@ public class Lienzo extends Canvas {
                 yPoints[k] = yPoints[k] + (int) val;
             }
             cont++;
+            //Para hacer el desface horizontal y alinear los hexagonos
             if (cont % 2 == 0) {
-//                xPoints[0] = 5;
-//                xPoints[1] = 10;
-//                xPoints[2] = 10;
-//                xPoints[3] = 5;
-//                xPoints[4] = 0;
-//                xPoints[5] = 0;
                 xPoints[0] = valHex;
                 xPoints[1] = 2 * valHex;
                 xPoints[2] = 2 * valHex;
@@ -116,12 +101,6 @@ public class Lienzo extends Canvas {
                 xPoints[4] = 0;
                 xPoints[5] = 0;
             } else {
-//                xPoints[0] = 12;
-//                xPoints[1] = 17;
-//                xPoints[2] = 17;
-//                xPoints[3] = 12;
-//                xPoints[4] = 7;
-//                xPoints[5] = 7;
                 xPoints[0] = valHex + (3 * valHex) / 2;
                 xPoints[1] = 2 * valHex + (3 * valHex) / 2;
                 xPoints[2] = 2 * valHex + (3 * valHex) / 2;
@@ -129,23 +108,22 @@ public class Lienzo extends Canvas {
                 xPoints[4] = 0 + (3 * valHex) / 2;
                 xPoints[5] = 0 + (3 * valHex) / 2;
             }
-
         }
-
-//        yPoints[0] = 0;
-//        yPoints[1] = 5;
-//        yPoints[2] = 10;
-//        yPoints[3] = 15;
-//        yPoints[4] = 10;
-//        yPoints[5] = 5;
         yPoints[0] = 0;
         yPoints[1] = valHex;
         yPoints[2] = 2 * valHex;
         yPoints[3] = 3 * valHex;
         yPoints[4] = 2 * valHex;
         yPoints[5] = valHex;
-
+//        this.printAll(gBuff);
+        
     }
+
+    @Override
+    public void update(Graphics g) {
+       paint(g);
+    }
+
 
     /**
      * @return the Matriz
@@ -188,16 +166,40 @@ public class Lienzo extends Canvas {
         int i = 0;
         while (i < generations) {
             hexatron.nextGen();
+            Graphics gr=this.getGraphics();
             this.repaint();
+            
             try {
                 Thread.sleep(500);
             } catch (InterruptedException ex) {
-                Logger.getLogger(Lienzo.class.getName()).log(Level.SEVERE, null, ex);
+                System.err.println("ERROR");
+                ex.printStackTrace();
             }
+            i++;
         }
+    }
+
+    public void run() {
+        startSimulation(getGenerations());
+    }
+
+    /**
+     * @return the generations
+     */
+    public int getGenerations() {
+        return generations;
+    }
+
+    /**
+     * @param generations the generations to set
+     */
+    public void setGenerations(int generations) {
+        this.generations = generations;
     }
     /**
      * Guarda la línea que se le pasa para dibujarla cuando se le indique
      * llamando a paint()
      */
+
+
 }
