b9f0aa92aec69f2c343a57af296c0e7a70eb2d66
laqp14@gmail.com <laqp14@gmail.com@23d8c49f-e412-a02e-2c2b-0a2be4b60954>
2011-02-21 04:20:02 +0000

    Animacion completa, y entradas para la misma

--- trunk/Hexatron/src/hexatron/MiDibujo.java
+++ trunk/Hexatron/src/hexatron/MiDibujo.java
@@ -2,11 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package hexatron;
 
 import Logica_Hexatron.Hexatron;
 import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.HeadlessException;
 import java.awt.Menu;
 import java.awt.MenuBar;
 import java.awt.MenuItem;
@@ -23,50 +24,98 @@ import javax.swing.JOptionPane;
  *
  * @author Ztiphen
  */
-public class MiDibujo extends JFrame{
+public class MiDibujo extends JFrame {
 
+    
     private Hexatron hexatron = new Hexatron();
     Lienzo lienzo = new Lienzo(getHexatron());
 
-    public void dibujar()
-    {
-    this.add(lienzo);
-    }
-    public void repintar(){
+    private static MenuBar crearMenuBar(final MiDibujo midibujo) throws HeadlessException {
+        MenuBar menubar = new MenuBar();
+        Menu menu = new Menu("automata");
+        //Cuadrar ancho y alto
+        MenuItem menuItem = new MenuItem("Dimenciones", new MenuShortcut(KeyEvent.VK_D));
+        menuItem.addActionListener(new ActionListener() {
 
+            public void actionPerformed(ActionEvent e) {
+                String sanch = JOptionPane.showInputDialog("# numero de celulas a lo ancho, actual");
+                int ancho = Integer.parseInt(sanch);
+                String salto = JOptionPane.showInputDialog("# numero de celulas a lo alto");
+                int alto = Integer.parseInt(salto);
+                midibujo.getHexatron().setAlto(alto);
+                midibujo.getHexatron().setAncho(ancho);
+                midibujo.getHexatron().poblar();
+                midibujo.repintar();
+                //                midibujo.repaint();
+            }
+        });
+        menu.add(menuItem);
+        //Cuadrar numero de generaciones
+        menuItem = new MenuItem("Generaciones", new MenuShortcut(KeyEvent.VK_G));
+        menuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String sgen = JOptionPane.showInputDialog("# numero de Generaciones Por Correr, actual:"+midibujo.lienzo.getGenerations());
+                midibujo.lienzo.setGenerations(Integer.parseInt(sgen));
+            }
+        });
+        menu.add(menuItem);
+
+        //Iniciar simulacion
+        menuItem = new MenuItem("Iniciar simulation", new MenuShortcut(KeyEvent.VK_I));
+        menuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //midibujo.startSimulation();
+                new Thread(midibujo.lienzo).start();
+            }
+        });
+        menu.add(menuItem);
+
+        //step
+        menuItem = new MenuItem("Paso", new MenuShortcut(KeyEvent.VK_P));
+        menuItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                midibujo.paso();
+            }
+        });
+        menu.add(menuItem);
+
+        //re pobloar
+        menuItem = new MenuItem("Repoblar", new MenuShortcut(KeyEvent.VK_R));
+        menuItem.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                midibujo.hexatron.poblar();
+                midibujo.repaint();
+            }
+        });
+        menu.add(menuItem);
+
+
+        menubar.add(menu);
+        return menubar;
+    }
+
+    public void dibujar() {
+        this.add(lienzo);
+    }
+
+    public void repintar() {
         lienzo.setHexatron(hexatron);
         lienzo.repaint();
     }
 
     public static void main(String args[]) {
-    int screenx =  Toolkit.getDefaultToolkit().getScreenSize().width;
-    int screeny = Toolkit.getDefaultToolkit().getScreenSize().height;
+        int screenx = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int screeny = Toolkit.getDefaultToolkit().getScreenSize().height;
 
-    final MiDibujo midibujo = new MiDibujo();
-      MenuBar menubar= new MenuBar();
-    Menu menu=new Menu("automata");
-    MenuItem menuItem=new MenuItem("Ancho", new MenuShortcut(KeyEvent.VK_A));
-    menuItem.addActionListener(new ActionListener() {            
-            public void actionPerformed(ActionEvent e) {
-                String sanch=JOptionPane.showInputDialog("# numero de celulas a lo ancho");
-                int ancho=Integer.parseInt(sanch);
-                 String salto=JOptionPane.showInputDialog("# numero de celulas a lo alto");
-                int alto=Integer.parseInt(salto);
-                midibujo.getHexatron().setAlto(alto);
-                midibujo.getHexatron().setAncho(ancho);
-                midibujo.getHexatron().poblar();
-                midibujo.repintar();
-                midibujo.repaint();
-            }
-        });
-    menu.add(menuItem);
-    menubar.add(menu);
-    midibujo.setMenuBar(menubar);
-    midibujo.dibujar();
-    midibujo.setBounds(0, 0,screenx,screeny-50);
-    midibujo.setVisible(true);
-    midibujo.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    midibujo.startSimulation();
+        MiDibujo midibujo = new MiDibujo();
+        MenuBar menubar = crearMenuBar(midibujo);
+        midibujo.setMenuBar(menubar);
+        midibujo.lienzo.setDoubleBuffered(true);
+        midibujo.dibujar();
+        midibujo.setBounds(0, 0, screenx, screeny - 50);
+        midibujo.setVisible(true);
+        midibujo.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     /**
@@ -76,8 +125,14 @@ public class MiDibujo extends JFrame{
         return hexatron;
     }
 
-    private void startSimulation() {
-        lienzo.startSimulation(20);
+    
+
+    @Override
+    public void update(Graphics g) {
+        paint(g);
     }
 
+    private void paso() {
+        lienzo.startSimulation(1);
+    }
 }
