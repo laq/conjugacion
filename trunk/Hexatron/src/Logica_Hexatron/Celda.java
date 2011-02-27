496c70bad469e6e166b93cea5ba3eb7627897add
laqp14@gmail.com <laqp14@gmail.com@23d8c49f-e412-a02e-2c2b-0a2be4b60954>
2011-02-27 08:28:04 +0000

    capa de concentracion

--- trunk/Hexatron/src/Logica_Hexatron/Celda.java
+++ trunk/Hexatron/src/Logica_Hexatron/Celda.java
@@ -10,10 +10,27 @@ package Logica_Hexatron;
  * @author Ztiphen
  */
 public class Celda {
-    
-   float concentracion;
+   public static float concentrationMax=1000;
+   private float concentration=(float)Math.random()*400;
    int x;
    int y;
+
+    /**
+     * @return the concentration
+     */
+    public float getConcentration() {
+        return concentration;
+    }
+
+    /**
+     * @param concentration the concentration to set
+     */
+    public void setConcentration(float concentration) {
+        if (concentration>Celda.concentrationMax){
+            concentration=Celda.concentrationMax;
+        }
+        this.concentration = concentration;
+    }
    
     
 
