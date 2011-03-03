343b887ec243fb167995303b620afd2c762731c6
laqp14@gmail.com <laqp14@gmail.com@23d8c49f-e412-a02e-2c2b-0a2be4b60954>
2011-03-03 03:41:55 +0000

    seteo en cero de la variable generacion

--- trunk/Hexatron/src/Logica_Hexatron/Constants.java
+++ trunk/Hexatron/src/Logica_Hexatron/Constants.java
@@ -11,7 +11,7 @@ package Logica_Hexatron;
  */
 public class Constants {
     //Additive prbabilities, the last one must be 1.
-    public static float conjugationProbability= 0.5f;
+    public static float conjugationProbability= 0.25f;
     public static float movementProbability=0.75f;//0.25f;
     public static float nothingProbability=1f;//0.25f;
 
